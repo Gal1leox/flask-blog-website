cdc042790f4bec52e48462ac908e182cf3608df0
DaniilKalts <kaltsdaniil@gmail.com>
2025-01-26 11:00:29 +0500

    feat(frontend): add navbar for home page

--- website/routes/home.py
+++ website/routes/home.py
@@ -1,9 +1,16 @@
+import base64
 from flask import Blueprint, render_template
 from flask_login import current_user
+from ..models import Admin
 
 home_bp = Blueprint("home", __name__, template_folder="../templates/home")
 
 
 @home_bp.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", user=current_user)
+    avatar_data = None
+    if current_user.is_authenticated:
+        admin = Admin.query.filter_by(created_at=current_user.created_at).first()
+        if admin and admin.avatar:
+            avatar_data = base64.b64encode(admin.avatar).decode("utf-8")
+    return render_template("index.html", avatar_data=avatar_data)
--- website/utils/convert_files.py
+++ website/utils/convert_files.py
@@ -3,10 +3,3 @@ def convert_image_to_binary(image_path):
 
     with open(image_path, "rb") as file:
         return file.read()
-
-
-def convert_binary_to_image(binary_data, output_name):
-    """Convert binary data into an image file."""
-
-    with open(binary_data, "wb") as file:
-        return file.write(binary_data)
